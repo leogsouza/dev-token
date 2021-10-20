8369e8a751a435ea0e9a4aa036a4e5980ff94dc0
Leonardo Gonçalves <ldsz@gft.com>
2021-10-20 16:57:35 -0300

    Initial commit

---
+++ migrations/1_deploy_devtoken.js
@@ -0,0 +1,8 @@
+// Make sure the DevToken contract is included by requiring it.
+const DevToken = artifacts.require("DevToken");
+
+// This is an async function, it will accept the Deployer account, the network, and eventual accounts.
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(DevToken, "DevToken", "DVTK", 18, "50000000000000000000000");
+  const devToken = await DevToken.deployed()
+};
